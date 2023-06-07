fadfbe1539cab3288cdfd8b913dab5a1e23ed7d9
Spillertb <Bradleyspillert@gmail.com>
2023-06-06 21:46:02 -0400

    Update simulate.py

--- src/simulate.py
+++ src/simulate.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from typing import List, Tuple
 
+BIT_DEPTH = 255
+
 MATRIX_WIDTH = 64
 MATRIX_HEIGHT = 32
 
@@ -19,8 +21,8 @@ def display_matrix(matrix: List[List[Tuple[int, int, int]]]) -> None:
     """"""
     y, x = np.indices(matrix.shape[:2])
 
-    # flatten tuples
-    colors = matrix.reshape(-1, 3) / 255.0
+    # flatten tuples into (r, g, b)
+    colors = matrix.reshape(-1, 3) / BIT_DEPTH
 
     _, ax = plt.subplots(figsize=(8, 4), dpi=150)
 
@@ -29,7 +31,7 @@ def display_matrix(matrix: List[List[Tuple[int, int, int]]]) -> None:
 
     ax.scatter(x.ravel(), y.ravel(), c=colors, s=12)
 
-    # Invert y axis to match the matrix representation
+    # led matrix starts at the top, plots start at the bottom
     ax.invert_yaxis()
 
     plt.show()
@@ -48,7 +50,7 @@ def draw_letter(
         for i in range(LETTER_WIDTH - 1, -1, -1):
             bit = (horizontal >> i) & 1
             if bit:
-                matrix[row][col] = (255, 0, 0) # dot color
+                matrix[row][col] = (BIT_DEPTH, 0, 0) # dot color
 
             col += 1
         row += 1
@@ -59,7 +61,7 @@ def draw_letter(
 # example
 if __name__ == "__main__":
     # create background of different colors
-    led_matrix = np.random.randint(200, 255, (MATRIX_HEIGHT, MATRIX_WIDTH, 3))
+    led_matrix = np.random.randint(200, BIT_DEPTH, (MATRIX_HEIGHT, MATRIX_WIDTH, 3))
 
     led_matrix = draw_letter(led_matrix, "b", 0, 0)
     led_matrix = draw_letter(led_matrix, "b", 0, LETTER_WIDTH)
