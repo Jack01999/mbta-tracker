1c462c83bc1cbb85257d0289693afd329da317c1
Spillertb <Bradleyspillert@gmail.com>
2023-06-08 22:54:46 -0400

    simple square

--- src/mbta-tracker.py
+++ src/mbta-tracker.py
@@ -97,8 +97,32 @@ def update_train_times():
             "------------------------------------------------------------------------------"
         )
 
+from displays.adafruit import AdaFruit
+
+
+class SimpleSquare(AdaFruit):
+    def __init__(self, *args, **kwargs):
+        super(SimpleSquare, self).__init__(*args, **kwargs)
+
+    def run(self):
+        offset_canvas = self.matrix.CreateFrameCanvas()
+        while True:
+            for x in range(0, self.matrix.width):
+                offset_canvas.SetPixel(x, x, 255, 255, 255)
+                offset_canvas.SetPixel(offset_canvas.height - 1 - x, x, 255, 0, 255)
+
+            for x in range(0, offset_canvas.width):
+                offset_canvas.SetPixel(x, 0, 255, 0, 0)
+                offset_canvas.SetPixel(x, offset_canvas.height - 1, 255, 255, 0)
+
+            for y in range(0, offset_canvas.height):
+                offset_canvas.SetPixel(0, y, 0, 0, 255)
+                offset_canvas.SetPixel(offset_canvas.width - 1, y, 0, 255, 0)
+            offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
 
 if __name__ == "__main__":
     # the main function of the program
+
+    simple_square = SimpleSquare()
 
     update_train_times()
