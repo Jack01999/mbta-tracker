1788014ca698a92e9d02ac55d7076c20b4cc4ae9
Spillertb <Bradleyspillert@gmail.com>
2023-07-08 16:14:14 -0400

    bin 4

--- src/programs/snake.py
+++ src/programs/snake.py
@@ -5,7 +5,7 @@ import src.data.state as state
 from copy import deepcopy
 from random import randrange
 
-BIN = 8
+BIN = 4
 # assert not BIN % 2, "Bin must be an even number"
 GAME_HEIGHT = math.floor(state.height / BIN)
 GAME_WIDTH = math.floor(state.width / BIN)
