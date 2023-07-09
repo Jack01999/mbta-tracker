abfbc8c0571fbd86d3ac30d8a6f47f21216d7dba
Spillertb <Bradleyspillert@gmail.com>
2023-07-08 22:36:39 -0400

    Update snake.py

--- src/programs/snake.py
+++ src/programs/snake.py
@@ -13,11 +13,11 @@ GAME_WIDTH = math.floor(state.WIDTH / BIN)
 # Colors
 SURFACE_CLR = (0, 0, 0)
 
-APPLE_CLR = (255, 0, 0)
+APPLE_CLR = (220, 50, 50)
 
 SNAKE_CLR = (50, 255, 50)
 
-HEAD_CLR = (75, 200, 75)
+HEAD_CLR = (25, 255, 75)
 
 # Game Settings
 INITIAL_SNAKE_LENGTH = 3
