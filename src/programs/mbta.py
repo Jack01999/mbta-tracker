b1e56541982378158ee81dc8262a4826a70681b8
Jack01999 <jackc2849@gmail.com>
2023-07-18 23:28:38 -0400

    Update mbta.py

--- src/programs/mbta.py
+++ src/programs/mbta.py
@@ -182,7 +182,7 @@ def get_arrival_times(stop: str, direction: int, limit: int):
         # If seconds is <= 60
         # Display "Approaching" (abbrev. "1 min")
         if seconds <= 60:
-            arrivalTimes.append(str(minutes) + "min")
+            arrivalTimes.append(str(seconds) + "min")
 
         # Round the seconds value to the nearest whole number of minutes, rounding up if exactly in-between.
         minutes = round(seconds / 60)
